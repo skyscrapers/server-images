983228c2b2494a742c3a6ab9710723619b7ec56f
Philip Dubois <hello@philipdubois.be>
2018-08-23 16:19:09 +0200

    Fix typo

--- k8s-baseimage/scripts/awsinspector.sh
+++ k8s-baseimage/scripts/awsinspector.sh
@@ -4,4 +4,4 @@ set -e
 # Download + install AWS Inspector
 curl -L https://inspector-agent.amazonaws.com/linux/latest/install -o /tmp/inspector_install
 cd /tmp
-sudo bash awsinspectorinstall
+sudo bash inspector_install
