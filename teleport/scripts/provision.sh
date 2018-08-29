db728740381cbb9a656acda659784b225f6bd392
iuri aranda <iuri.aranda@gmail.com>
2018-08-29 15:04:05 +0200

    Fix certbot installation in teleport image (#6)
    Apparently certbot uses python 3 when executed inside the cronjob, and 
    we were previously installing the route53 plugin on python 2.7

--- teleport/scripts/provision.sh
+++ teleport/scripts/provision.sh
@@ -15,8 +15,7 @@ sudo ./teleport/install
 # Install certbot
 sudo add-apt-repository -y ppa:certbot/certbot
 sudo apt-get update
-sudo apt-get install -y certbot python-pip
-sudo pip install certbot-dns-route53
+sudo apt-get install -y certbot python3-certbot-dns-route53
 
 # Download Cloudwatch logs agent and its dependencies. Setup is for the bootstrap phase
 sudo curl -o /root/awslogs-agent-setup.py https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py
