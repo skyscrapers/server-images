c36d6c9851d376c7809f0a85268c0badb14fea42
Philip Dubois <hello@philipdubois.be>
2020-04-16 12:27:18 +0200

    Cleanup unused image

--- k8s-baseimage/scripts/awsinspector.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# Download + install AWS Inspector
-curl -L https://inspector-agent.amazonaws.com/linux/latest/install -o /tmp/inspector_install
-cd /tmp
-sudo bash inspector_install
