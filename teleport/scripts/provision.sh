06dbad6ddcd74de031f6603c2c723de7dfcb8aaf
iuri aranda <iuri@skyscrapers.eu>
2020-06-04 17:36:05 +0200

    Update teleport image and add cleanup script
    - Switched teleport image from ubuntu to Amazon Linux 2
    - Updated teleport to the latest version available (4.2.0)
    - Add a common cleanup script

--- teleport/scripts/provision.sh
+++ teleport/scripts/provision.sh
@@ -2,24 +2,20 @@
 
 set -e
 
-# Apt upgrade
-sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+cd /tmp
+
+sudo wget -r --no-parent -A 'epel-release-*.rpm' http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/
+sudo rpm -Uvh dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-*.rpm
+sudo yum-config-manager --enable epel
+
+sudo yum update -y
 
 # Install teleport
-cd /tmp
-curl -sL `echo $TELEPORT_DOWNLOAD_URL_FORMAT | sed -e "s/%version%/$TELEPORT_VERSION/g"` > teleport.tar.gz
-sudo tar -xzf teleport.tar.gz
-sudo ./teleport/install
+sudo curl -sL `echo $TELEPORT_DOWNLOAD_URL_FORMAT | sed -e "s/%version%/$TELEPORT_VERSION/g"` > teleport.rpm
+sudo yum install -y teleport.rpm
 
 # Install certbot
-sudo add-apt-repository -y ppa:certbot/certbot
-sudo apt-get update
-sudo apt-get install -y certbot python3-certbot-dns-route53
+sudo yum install -y certbot python2-certbot-dns-route53
 
 # Download Cloudwatch logs agent and its dependencies. Setup is for the bootstrap phase
-sudo curl -o /root/awslogs-agent-setup.py https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py
-sudo curl -o /root/AgentDependencies.tar.gz https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/AgentDependencies.tar.gz
-
-# Cleanup
-sudo apt-get -y autoremove
+sudo yum install -y awslogs
