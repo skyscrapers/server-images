862ae79a889ae91fa78b26d0b8d1b4e20911de4f
Philip Dubois <hello@philipdubois.be>
2021-06-17 17:12:46 +0200

    Allow to disable asw inspector (#19)

--- eks-baseimage/scripts/awsinspector.sh
+++ eks-baseimage/scripts/awsinspector.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-# Download + install AWS Inspector
-curl -L https://inspector-agent.amazonaws.com/linux/latest/install -o /tmp/inspector_install
-cd /tmp
-sudo bash inspector_install
+if $AWS_INSPECTOR_ENABLED; then
+  # Download + install AWS Inspector
+  curl -L https://inspector-agent.amazonaws.com/linux/latest/install -o /tmp/inspector_install
+  cd /tmp
+  sudo bash inspector_install
+fi
